e1ca6d865d93c0715d5c1aa612cc04785b0c20b8
Gentley <gentleyrakotomalala@gmail.com>
2025-11-19 11:34:39 +0100

    Test hook

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
-from app import app
+from app import app  # noqa: E402
 
 
 @pytest.fixture
